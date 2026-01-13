c6091c26f663b6e45f9b4590869e90a245b09de8
leoetchebarne-ship-it <leoetchebarne@gmail.com>
2026-01-12 22:54:29 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Cargando...</div>;
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <p>Cargando comunidad...</p>
+    </div>
+  );
 }
