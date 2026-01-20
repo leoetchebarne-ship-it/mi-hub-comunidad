27ffe889defee789871826d72d2de12d1c297768
leoetchebarne-ship-it <leoetchebarne@gmail.com>
2026-01-20 00:40:10 -0300

    Update index.ts

--- app/experiences/[experienceId]/_v0-imports/index.ts
+++ app/experiences/[experienceId]/_v0-imports/index.ts
@@ -1,1 +1,5 @@
-
+export * from './FocusTimer';
+export * from './CommunityPanel';
+export * from './AchievementsPanel';
+export * from './RightDrawer';
+export * from './ProjectLab'; // ¡No olvides exportar el nuevo cerebro!
